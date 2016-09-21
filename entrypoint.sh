61bcf91287f9aa26c97eb975aa639c40d2ea94f0
David Kotalik <david.kotalik@ackee.cz>
2016-09-21 14:23:16 +0200

    Edit entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -eo pipefail
 
-echo ${CRON_SCHEDULE_1}
-
-#for i in 1 2 3 4 5 6 7 8
-#do
-#  cronSchVar=CRON_SCHEDULE_$i
-#  cronJobVar=CRON_JOB_$i
-#  if [ -z "${!cronSchVar}" -o -z "${!cronJobVar}" ]; then
-#    echo "Cron $i not specified, moving on."
-#  else
-#    # add a cron job
-#    echo "${!cronSchVar} root ${cronJobVar}" >> /etc/crontab
-#  fi
-#done
+for i in 1 2 3 4 5 6 7 8
+do
+  cronSchVar=CRON_SCHEDULE_$i
+  cronJobVar=CRON_JOB_$i
+  if [ -z "${!cronSchVar}" -o -z "${!cronJobVar}" ]; then
+    echo "Cron $i not specified, moving on."
+  else
+    # add a cron job
+    echo "${!cronSchVar} root ${cronJobVar}" >> /etc/crontab
+  fi
+done
 
 crontab /etc/crontab
 
